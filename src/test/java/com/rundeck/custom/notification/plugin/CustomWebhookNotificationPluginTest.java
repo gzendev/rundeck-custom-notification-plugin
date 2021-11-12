205345e0291048bd04248fa334b723b206bfdcb8
gcespedes <gustavo.cespedes@gmail.com>
2021-11-12 13:05:34 -0300

    Successfull unit test added

--- src/test/java/com/rundeck/custom/notification/plugin/CustomWebhookNotificationPluginTest.java
+++ src/test/java/com/rundeck/custom/notification/plugin/CustomWebhookNotificationPluginTest.java
@@ -6,30 +6,30 @@ import java.util.Map;
 import org.junit.Test;
 
 public class CustomWebhookNotificationPluginTest {
-  
+
   private CustomWebhookNotificationPlugin plugin;
   private Map<String, Object> config = new HashMap<String, Object>();
   private Map<String, Object> executionData = new HashMap<String, Object>();
   private String trigger;
-  
+
   private String webhookUrl;
   private String contentType;
   private String requestMethod;
   private String messageBody;
-  
+
   private boolean ret;
-  
+
   private static final String METHOD_GET = "GET";
   private static final String METHOD_POST = "POST";
   private static final String METHOD_PUT = "PUT";
   private static final String METHOD_DELETE = "DELETE";
-  
+
   private static final String CONTENT_TYPE_APPLICATION_JSON = "application/json";
   private static final String CONTENT_TYPE_APPLICATION_XML = "application/xml";
   private static final String CONTENT_TYPE_TEXT_HTML = "text/html";
   private static final String CONTENT_TYPE_APPLICATION_URLENCODED = "application/x-www-form-urlencoded";
-  
-  
+
+
   private void settingAttrs(String webhookUrl, String requestMethod, String contentType, String messageBody) {
     this.webhookUrl = webhookUrl;
     this.requestMethod = requestMethod;
@@ -37,7 +37,7 @@ public class CustomWebhookNotificationPluginTest {
     this.messageBody = messageBody;
     this.trigger = "success";
   }
-  
+
   @Test
   public void canHandleEmptyUrl() throws CustomMessageException {
     settingAttrs("", METHOD_GET, CONTENT_TYPE_APPLICATION_JSON, "");
@@ -45,7 +45,7 @@ public class CustomWebhookNotificationPluginTest {
     this.ret = this.plugin.postNotification(trigger, executionData, config);
     assertFalse(ret);
   }
-  
+
   @Test
   public void canHandleBadUrl() throws CustomMessageException {
     settingAttrs("www.test.com", METHOD_GET, CONTENT_TYPE_APPLICATION_JSON, "");
@@ -53,7 +53,7 @@ public class CustomWebhookNotificationPluginTest {
     this.ret = this.plugin.postNotification(trigger, executionData, config);
     assertFalse(ret);
   }
-  
+
   @Test
   public void callExistingUrl() {
     settingAttrs("https://www.google.com", METHOD_GET, CONTENT_TYPE_APPLICATION_JSON, "");
@@ -61,7 +61,7 @@ public class CustomWebhookNotificationPluginTest {
     this.ret = this.plugin.postNotification(trigger, executionData, config);
     assertTrue(ret);
   }
-  
+
   @Test
   public void callNotExistingUrl() {
     settingAttrs("http://test.com", METHOD_GET, CONTENT_TYPE_APPLICATION_JSON, "");
@@ -69,7 +69,7 @@ public class CustomWebhookNotificationPluginTest {
     this.ret = this.plugin.postNotification(trigger, executionData, config);
     assertFalse(ret);
   }
-  
+
   @Test
   public void canHandleEmptyContentType() {
     settingAttrs("https://www.google.com", METHOD_POST, "", "Body Messages");
@@ -77,7 +77,7 @@ public class CustomWebhookNotificationPluginTest {
     this.ret = this.plugin.postNotification(trigger, executionData, config);
     assertFalse(ret);
   }
-  
+
   @Test
   public void canHandleNotExistingContentType() {
     settingAttrs("https://www.google.com", METHOD_POST, "123", "Body Messages");
@@ -85,7 +85,7 @@ public class CustomWebhookNotificationPluginTest {
     this.ret = this.plugin.postNotification(trigger, executionData, config);
     assertFalse(ret);
   }
-  
+
   @Test
   public void canHandleEmptyRequestMethod() {
     settingAttrs("https://www.google.com", "", CONTENT_TYPE_APPLICATION_JSON, "");
@@ -93,7 +93,7 @@ public class CustomWebhookNotificationPluginTest {
     this.ret = this.plugin.postNotification(trigger, executionData, config);
     assertFalse(ret);
   }
-  
+
   @Test
   public void canHandleNotExistingRequestMethod() {
     settingAttrs("https://www.google.com", "123", CONTENT_TYPE_APPLICATION_JSON, "");
@@ -101,7 +101,7 @@ public class CustomWebhookNotificationPluginTest {
     this.ret = this.plugin.postNotification(trigger, executionData, config);
     assertFalse(ret);
   }
-  
+
   @Test
   public void canHandleEmptyMessageBody() {
     settingAttrs("https://www.google.com", METHOD_PUT, CONTENT_TYPE_APPLICATION_JSON, "");
@@ -109,5 +109,13 @@ public class CustomWebhookNotificationPluginTest {
     this.ret = this.plugin.postNotification(trigger, executionData, config);
     assertFalse(ret);
   }
-  
+
+  @Test
+  public void allRequiredFieldsCompleted () {
+    settingAttrs("https://jsonplaceholder.typicode.com/posts", METHOD_POST, CONTENT_TYPE_APPLICATION_JSON, "{\"rundeck\": \"success\"}");
+    this.plugin = new CustomWebhookNotificationPlugin(webhookUrl, requestMethod, contentType, messageBody);
+    this.ret = this.plugin.postNotification(trigger, executionData, config);
+    assertTrue(ret);
+  }
+
 }
